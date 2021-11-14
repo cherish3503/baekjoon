fcd99f3b50ac3fbe85eb9895e80c09744df4adf8
cherish3503 <90964197+cherish3503@users.noreply.github.com>
2021-11-14 19:38:01 +0900

    Create sangsu.py

---
+++ sangsu.py
@@ -0,0 +1,9 @@
+#baekjoon_2908
+num = input().split()
+
+
+sangsu0 = int(''.join(reversed(num[0])))
+sangsu1 = int(''.join(reversed(num[1])))
+sangsu = sangsu0 if (sangsu0 > sangsu1) else sangsu1
+
+print(sangsu)
