5f504cf92e89d2dfa172d9c02b743b4662dfb75d
cherish3503 <90964197+cherish3503@users.noreply.github.com>
2021-12-25 22:05:04 +0900

    Update josephusProblem.py

--- josephusProblem.py
+++ josephusProblem.py
@@ -1,16 +1,17 @@
 #Baekjoon_1158
 
-def josephusProblem(arr,n):
+def josephusProblem(n,k):
+    arr = list(range(1,1+n))
     josephus = list()
     tmp = 0
     for i in range(len(arr)):
-        tmp = (tmp + n -1) % len(arr)
+        tmp = (tmp + k -1) % len(arr)
         josephus.append(arr.pop(tmp))
 
     return josephus
 
 n,k = map(int,input().split())
-result = josephusProblem(list(range(1,1+n)),k)
+result = josephusProblem(n,k)
 
 print('<', end = '')
 for i,e in enumerate(result):
