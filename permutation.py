207f15cd24baea0c74c56c8b616d7e5260214ca7
cherish3503 <90964197+cherish3503@users.noreply.github.com>
2021-12-31 22:41:35 +0900

    Update and rename combine.py to permutation.py

--- permutation.py
+++ permutation.py
@@ -4,7 +4,7 @@ n, m = map(int, input().split())
 visited = [False] * n
 arr = list()
 
-def combine(n, m, depth):
+def permutation(n, m, depth):
   if depth == m:
     # m개를 모두 탐색
     print(' '.join(map(str,arr)))
@@ -17,10 +17,10 @@ def combine(n, m, depth):
         # ex) visited[0] = 1이 포함되어있는지
         visited[i] = True
         arr.append(i+1)
-        combine(n,m,depth+1)
+        permutation(n,m,depth+1)
         # depth당 한개의 원소를 추가
         visited[i] = False
         arr.pop()
         # 한 번 배열을 완성한 후 한 depth씩 올라가면서 체크
 
-combine(n,m,0)
+permutation(n,m,0)
