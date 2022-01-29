3f034a0eb53e439567757a68fdecc4936c349111
cherish3503 <90964197+cherish3503@users.noreply.github.com>
2022-01-29 21:32:41 +0900

    Update continued_sum.py

--- continued_sum.py
+++ continued_sum.py
@@ -9,6 +9,7 @@ dp = [0] * n
 dp[0] = num[0]
 for i in range(1,len(num)):
   dp[i] = max(dp[i-1] + num[i], num[i])
+  # 구간 합이 끊겼다는 것은 그 구간의 합이 0보다 작다는 뜻 == 그 구간을 더하는것에 이득이 없음
   # num[i-1]을 포함한 구간합의 최대 값 : dp[i-1]
   # num[i]를 포함한 구간합의 최대 : dp[i]
 
