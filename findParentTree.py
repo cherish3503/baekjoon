414edf744943125ebb1863462ac562fc4f5ecade
cherish3503 <90964197+cherish3503@users.noreply.github.com>
2022-02-02 17:22:27 +0900

    temp wrong answer

--- findParentTree.py
+++ findParentTree.py
@@ -1,6 +1,8 @@
 # Baekjoon_11725
+# dict를 사용한 트리 구현
 
 import sys
+sys.setrecursionlimit(10**6)
 
 class Node:
   def __init__(self, data, left_node, right_node):
@@ -9,36 +11,46 @@ class Node:
     self.right_node = right_node
 
 def findParentTree(node):
+  # 자식노드의 부모 노드를 기록
   if node.left_node != None:
-    parent_node[node.left_node] = node.data
-    findParentTree(node.left_node)
+    parent_node[node.left_node.data] = node.data
+    if node.left_node.data in tree:
+      findParentTree(node.left_node)
   if node.right_node != None:
-    parent_node[node.right_node] = node.data
-    findParentTree(node.right_tree)
+    parent_node[node.right_node.data] = node.data
+    if node.right_node.data in tree:
+      findParentTree(node.right_node)
 
-def makeTree(data):
+def makeTree(node):
   left, right = None, None
-  for i in graph[data]:
+  for i in graph[node.data]:
     if i not in tree:
       if left == None:
-        left = i
+        left = Node(i, None, None)
+        tree[node.data].left_node = left
+        tree[i] = left
       else:
-        right = i
+        right = Node(i, None, None)
+        tree[node.data].right_node = right
+        tree[i] = right
   
-  if not (left == None and right == None):
-    tree[data] = Node(data, left, right)
+  if left != None:
+    makeTree(left)
+  if right != None:
+    makeTree(right)
 
 
 n = int(input())
-tree = dict()
+tree = dict() # dict를 사용한 트리
 graph = [[] for _ in range(n+1)]
 for _ in range(n-1):
   i1, i2 = map(int, sys.stdin.readline().split())
   graph[i1].append(i2)
   graph[i2].append(i1)
 
-makeTree(1)
-
-parent_node = []*(n+1)
-findParentTree(tree(1))
-print(tree)
+tree[1] = Node(1, None, None)
+makeTree(tree[1]) # 1을 루트로 하는 트리 생성
+parent_node = [None]*(n+1)
+findParentTree(tree[1]) # 루트부터 자식노드의 부모 노드를 기록
+for i in range(2,len(parent_node)):
+  print(parent_node[i])
