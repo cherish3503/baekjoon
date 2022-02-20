c963a36076874244f2e092199e36fb8ed8287f8e
cherish3503 <90964197+cherish3503@users.noreply.github.com>
2022-02-20 20:28:30 +0900

    Update ripeTomato.py
    pop -> popleft # queue
    queue를 불필요하게 새로 만들지 않음 -> 메모리 10%수준으로 감소

--- bfs/#7569/ripeTomato.py
+++ bfs/#7569/ripeTomato.py
@@ -9,30 +9,22 @@ dx = [1,0,-1,0,0,0]
 dy = [0,-1,0,1,0,0]
 dz = [0,0,0,0,1,-1]
 
-def ripeTomato(tomato, queue, day):
-  global result
-  if checkTomato(tomato):
-    result = day
-    return
-  if len(queue) == 0:
-    # queue가 비어 있을경우 모두 익지 못하는 상황
-    result = -1
-    return
-  
-  queue_n = deque()
-
+def ripeTomato(tomato, queue):
+  day = 0
   while(queue):
-    # 새로 익은 토마토를 queue_n에 담음
-    z, y, x = queue.pop()
+    z, y, x, day = queue.popleft()
     for i in range(len(dx)):
       nz,ny,nx = z+dz[i], y+dy[i], x+dx[i]
       if nz<0 or nz >=len(tomato) or ny<0 or ny >= len(tomato[0]) or nx<0 or nx >= len(tomato[0][0]):
         continue
       if tomato[nz][ny][nx] == 0:
         tomato[nz][ny][nx] = 1
-        queue_n.append((nz,ny,nx))
+        queue.append((nz,ny,nx,day+1))
   
-  ripeTomato(tomato, queue_n, day+1)
+  if checkTomato(tomato):
+    return day
+  else:
+    return -1
   
 
 def checkTomato(tomato):
@@ -51,7 +43,7 @@ def startTomato(tomato):
     for row in range(len(tomato[h])):
       for col in range(len(tomato[h][row])):
         if tomato[h][row][col] == 1:
-          start.append((h,row,col))
+          start.append((h,row,col,0))
   return start
 
 m,n,h = map(int, sys.stdin.readline().split())
@@ -62,5 +54,4 @@ for _ in range(h):
     box.append(list(map(int, sys.stdin.readline().split())))
   tomato.append(box)
 
-ripeTomato(tomato, startTomato(tomato), 0)
-print(result)
+print(ripeTomato(tomato, startTomato(tomato)))
