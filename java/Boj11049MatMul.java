79ad40aa83d1d9b0b73e879ccaea3a680fdf3c27
cherish3503 <90964197+cherish3503@users.noreply.github.com>
2026-02-05 15:47:08 +0900

    Add files via upload

--- java/Boj11049MatMul.java
+++ java/Boj11049MatMul.java
@@ -16,24 +16,28 @@ public class Boj11049MatMul {
 			} else	st.nextToken();
 			arr[i+1] = (Integer.parseInt(st.nextToken()));
 		}
-//		System.out.println(Arrays.toString(arr));
+		System.out.println(minMul(arr, dp, 0, N));
+		System.out.println();
 	}
 	
 	
 	private static int minMul(int[] arr, int[][] dp, int s, int e) {
-		int size = arr.length;
-		if(size<3) {
+		int size = e-s+1;
+		if(size<=2) {
 			return 0;
 		}
 		if(dp[s][e] != 0) {
 			return dp[s][e];
 		}
 		
-		for(int i=2; i<size; ++i) {
-			
+		int min = Integer.MAX_VALUE;
+		for(int i=s+1; i<e; ++i) {
+			int res = minMul(arr, dp, s, i) + minMul(arr, dp, i, e) + arr[s]*arr[i]*arr[e];
+			min = Math.min(min, res);
 		}
 		
-		return 0;
+		dp[s][e] = min;
+		return min;
 	}
 	
 }
