33ecae1279994e2830d7ba3700b1c9cb0997ae80
cherish3503 <cherish3503@gmail.com>
2026-03-04 14:39:46 +0900

    1202 fin

--- java/Swea1202Jewelry.java
+++ java/Swea1202Jewelry.java
@@ -15,8 +15,10 @@ public class Swea1202Jewelry {
 		
 		for(int i=0; i<N; ++i) {
 			st = new StringTokenizer(br.readLine());
-			jewelrys[i][0] = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			jewelrys[i][0] = M;
 			jewelrys[i][1] = Integer.parseInt(st.nextToken());
+
 		}
 		
 		for(int i=0; i<K; ++i) {
@@ -24,18 +26,29 @@ public class Swea1202Jewelry {
 		}
 		Arrays.sort(bags);
 		
-		for(int i=0; i<K; ++i) {
-			bags[i] = Integer.parseInt(br.readLine());
+		Arrays.sort(jewelrys, (e1,e2) ->Integer.compare(e1[0],e2[0]));
+		//무게 내림차순 정렬
+		
+
+		PriorityQueue<int[]> pq = new PriorityQueue<>((e1,e2)-> Integer.compare(e2[1],e1[1]));
+		long result = 0;
+		int idx = 0;
+		for(int b : bags) {
+			while(idx<N) {
+				if(jewelrys[idx][0] > b) break;
+				pq.offer(jewelrys[idx]);
+				idx++;
+			}
+			if(!pq.isEmpty()) result += pq.poll()[1];
 		}
 		
-		Arrays.sort(jewelrys, (e1,e2) ->{
-			if(e1[0] == e2[0]) return Integer.compare(e1[1],e2[1]);
-			return Integer.compare(e1[0],e2[0]);
-		});
+		System.out.println(result);
 	}
 }
 /*
  * 
 2) 작은 가방 순으로 정렬
 그 가방에 들어 갈 수 있는 가장 큰 가치
+
+가방 내림차순으로 처리하면서 pq(value내림차순)에 가방보다 작은 보석 넣기 
  */
